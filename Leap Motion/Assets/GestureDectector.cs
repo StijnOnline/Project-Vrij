1429c317bcd98763b7f97f4b8e9c9503d8354d0a
StijnOnline <stijnvandeijzen@gmail.com>
2019-05-12 15:13:26 +0200

    Initial Commit with working tests

---
+++ Leap Motion/Assets/GestureDectector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestureDectector : MonoBehaviour
+{
+    public void Steen()
+    {
+        Debug.Log("Steen");
+    }
+
+    public void Papier()
+    {
+        Debug.Log("Papier");
+    }
+
+    public void Schaar()
+    {
+        Debug.Log("Schaar");
+    }
+}
---
+++ Leap Motion/Assets/platform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platform : MonoBehaviour
+{
+    public Transform hand1;
+    public Transform hand2;
+
+
+    void Update()
+    {
+        //consider trying rigidbody movement
+
+        if (hand1.gameObject.active && hand2.gameObject.active)
+        {
+
+            Debug.DrawLine(hand1.position, hand2.position, Color.red, Time.deltaTime);
+            Debug.DrawLine(transform.position, transform.position + (hand1.forward + hand2.forward) / 2, Color.red, Time.deltaTime);
+
+
+            transform.position = (hand1.position + (hand2.position - hand1.position) / 2);
+            transform.rotation = Quaternion.LookRotation(hand2.position - hand1.position, (hand1.up + hand2.up) / 2);
+
+            
+            
+        }
+    }
+}
