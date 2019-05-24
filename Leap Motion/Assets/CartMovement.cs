37334884c27e6857a690ed8e7756aee0e7598513
StijnOnline <stijnvandeijzen@gmail.com>
2019-05-24 17:29:58 +0200

    Added more to Shop: Spawning of objects and cart movement (with box colliders on cart)

---
+++ Leap Motion/Assets/CartMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartMovement : MonoBehaviour
+{
+    public Vector3 speed = new Vector3(0.5f,0,0);
+    Rigidbody rididB;
+
+    void Start()
+    {
+        rididB = GetComponent<Rigidbody>();
+        rididB.velocity = speed;
+    }
+
+}
---
+++ Leap Motion/Assets/Project/Winkel/ProductSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductSpawner : MonoBehaviour
+{
+    public List<GameObject> products;
+    public int count = 5;
+    public Vector3 offset = new Vector3(0.2f,0,0);
+
+    void Start()
+    {
+        Vector3 pos = new Vector3(0,0,0); 
+        for (int i = 0; i<count; i++)
+        {
+            GameObject product = products[Random.Range(0, products.Count)];
+            GameObject ob = Instantiate(product, transform);
+            ob.transform.position = transform.position + pos;
+            pos += offset;
+        }
+    }
+
+}
