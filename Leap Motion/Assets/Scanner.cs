ad2110c06e14126bb0882fb0b1785ced31341026
StijnOnline <stijnvandeijzen@gmail.com>
2019-06-17 15:55:25 +0200

    Basis Kassa werkt

--- Leap Motion/Assets/Scanner.cs
+++ Leap Motion/Assets/Scanner.cs
@@ -7,13 +7,23 @@ public class Scanner : MonoBehaviour
 {
     public TextMeshProUGUI productText;
     public TextMeshProUGUI scoreText;
-    public int totalScore;
+    public AudioSource AudioSource;
+
+    public AudioClip bleep;
+
+    int totalScore;
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Product")
         {
-            productText.SetText(other.gameObject.name);
+            AudioSource.PlayOneShot(bleep);
+            productText.SetText(other.gameObject.name.Split('(')[0]);
             scoreText.SetText("Total: " + totalScore);
         }
+    }
+
+    public IEnumerator Check()
+    {
+        yield return 0;
     }
 }
