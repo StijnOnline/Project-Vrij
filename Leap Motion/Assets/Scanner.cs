2d373c0fea237be0cb160f8d8e4fb4670db5f6e6
StijnOnline <stijnvandeijzen@gmail.com>
2019-06-17 15:44:07 +0200

    WIP Kassa

---
+++ Leap Motion/Assets/Scanner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Scanner : MonoBehaviour
+{
+    public TextMeshProUGUI productText;
+    public TextMeshProUGUI scoreText;
+    public int totalScore;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Product")
+        {
+            productText.SetText(other.gameObject.name);
+            scoreText.SetText("Total: " + totalScore);
+        }
+    }
+}
